4857f02954ab93b7d2985678581ce9facb32265f
snikidev <n.kakuev@gmail.com>
2022-06-22 19:25:15 +0100

    fix: revert api response change

--- pages/api/companies.ts
+++ pages/api/companies.ts
@@ -1,19 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { LIMIT } from "consts";
-import data from "../../data/companies.json";
-
-const companiesString = JSON.stringify(data);
-const companies = JSON.parse(companiesString);
-
-type Data = {
-  companies?: any;
-  error?: string;
-};
+import companies from "../../data/companies.json";
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<any>
 ) {
   res.status(200).json(companies);
 }
